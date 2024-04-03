b277c4d2d4509f9adc36316be483a08073e71031
Osama-Moh <usama.nasser21@gmail.com>
2024-04-03 17:04:52 +0200

    CTF-1 Done

--- CTF/CTF-1 (Cryptanalysis)/CTF1.py
+++ CTF/CTF-1 (Cryptanalysis)/CTF1.py
@@ -1,19 +1,59 @@
 from collections import Counter
-import re
+
+f = open("statistics.txt", "w")
 
 with open('encrypted_text.txt', 'r') as file:
     encrypted_text = file.read()
 
-encrypted_text = re.sub(r'[^a-zA-Z]', '', encrypted_text).upper()
-letter_freq = Counter(encrypted_text)
-sorted_freq = letter_freq.most_common()
+character_count = Counter(encrypted_text)
 
+for char, count in character_count.items():
+        print(f"Character '{char}' appears {count} times.")
+        f.write(f"Character '{char}' appears {count} times.\n\n")
+        print() 
+f.close()
 
-english_freq = 'etoahinsrdlcumwfgypbvkjxqz'
-mapping = {}
-for i in range(len(sorted_freq)):
-    mapping[sorted_freq[i][0]] = english_freq[i]
+# Define a dictionary to map the letters to their replacements
+letter_mapping = {
+    'I': 'e',
+    'R': 'i',
+    'V': 's',
+    'M': 'n',
+    'K': 't',
+    'P': 'o',
+    'H': 'h',
+    'E': 'a',
+    'B': 'r',
+    'S': 'g',
+    'Q': 'v',
+    'O': 'd',
+    'X': 'u',
+    'T': 'b',
+    'W': 'y',
+    'Y': 'w',
+    'N': 'f',
+    'C': 'l',
+    'G': 'p',
+    'U': 'm',
+    'A': 'c',
+    'L': 'k',
+    'J': 'x',
+    'Z': 'z',
+    'D': 'j',
+    'F': 'q',
+}
 
-decrypted_text = ''.join(mapping.get(char, char) for char in encrypted_text)
-print(sorted_freq)
+f = open("decrypted_text.txt", "w")
+
+# Replace the letters in the encrypted text
+decrypted_text = ''
+for char in encrypted_text:
+    if char in letter_mapping:
+        decrypted_text += letter_mapping[char]
+    else:
+        decrypted_text += char
+
+f.write(decrypted_text)
+f.close()
+
 print(decrypted_text)
