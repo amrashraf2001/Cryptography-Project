43e068905c585a5f10db65b52c29bb09f1e31ef2
Amr Ashraf <102065583+amrashraf2001@users.noreply.github.com>
2024-04-03 21:40:18 +0200

    Update CTF4.py

--- CTF/CTF-4 (Bit Shifting)/CTF4.py
+++ CTF/CTF-4 (Bit Shifting)/CTF4.py
@@ -1,14 +1,3 @@
 with open('bits.txt', 'rb') as file:
     flag = file.read()
 
-decoded = []
-
-for i in range(0, len(flag), 2):
-    # Two bytes to integer.
-    btoi = flag[i] << 8 | flag[i+1]
-    # First byte.
-    decoded.append(chr(btoi >> 8))
-    # Second byte.
-    decoded.append(chr(btoi & 255))
-
-print("".join(decoded))
