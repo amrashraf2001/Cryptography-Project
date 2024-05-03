e7037b5945acd1747d13141f4dcdf2ff14fceaa8
Osama-Moh <usama.nasser21@gmail.com>
2024-05-04 00:35:32 +0300

    version 2

--- Chat/Gamal.py
+++ Chat/Gamal.py
@@ -1,4 +1,6 @@
 import random
+from math import gcd
+import hashlib
 
 def is_primitive_root(g, p):
     seen = set()
@@ -14,15 +16,26 @@ def find_primitive_root(p):
         if is_primitive_root(g, p):
             return g
    
+def generate_random_k(q):
+    while True:
+        k = random.randint(1, q - 1)
+        if gcd(k, q - 1) == 1:
+            return k
+
 q = 71
 alpha = find_primitive_root(q)
 
 #Key Generation
-Xa = random.randint(1, q - 1)
+Xa = random.randint(2, q - 2)
 Ya = pow(alpha, Xa, q)
 PrivateKey = Xa
 PublicKey = Ya
 
 print(f"Public Key: {PublicKey}")
 print(f"Private Key: {PrivateKey}")
+
+k = generate_random_k(q)
+K = pow(Ya, k, q) #lecture
+K = pow(alpha, k, q) #GPT
+
 
