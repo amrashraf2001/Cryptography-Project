252030a4d6c65767c0c600c45650703ac5f717db
Osama-Moh <usama.nasser21@gmail.com>
2024-05-09 19:51:00 +0300

    CTFs Done

--- CTF/CTF-4 (Bit Shifting)/CTF4.py
+++ CTF/CTF-4 (Bit Shifting)/CTF4.py
@@ -1,3 +1,24 @@
-with open('bits.txt', 'rb') as file:
-    flag = file.read()
+def read_file(file_name):
+    with open(file_name, 'r', encoding='ISO-8859-1') as file:
+        data = file.read()
+    return data
+
+def text_to_binary(text):
+    binary_data = ''.join(format(ord(char), '08b') for char in text)
+    return binary_data
+
+def binary_to_text(binary_data):
+    text = ''.join(chr(int(binary_data[i:i+8], 2)) for i in range(0, len(binary_data), 8))
+    return text
+
+def one_time_shift(binary_data):
+    shifted_data = binary_data[1:] + binary_data[0]
+    return shifted_data
+
+file_name = 'bits.txt'
+original_data = read_file(file_name)
+binary_data = text_to_binary(original_data)
+shifted_data = one_time_shift(binary_data)
+modified_text = binary_to_text(shifted_data)
+print("Modified Text:", modified_text)
 
